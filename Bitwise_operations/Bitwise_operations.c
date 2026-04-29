b71822cb0db44452f471043dc1aa2045241ad5e9
arif__shaik___ <72382780+arifshaik123@users.noreply.github.com>
2026-04-29 14:36:35 +0530

    Update Bitwise_operations.c

--- Bitwise_operations/Bitwise_operations.c
+++ Bitwise_operations/Bitwise_operations.c
@@ -1,34 +1,39 @@
 #include<stdio.h>
 
-int set_bit(int num);
+int set_bit(int,int);
+
+
+
 //THIS FUNCTION PERFORMS TO SET THE PARTICULAR BIT POSITION 
-int set_bit(int num)
+int set_bit(int num,int pos)
 {
-    int bitpos;
-
-    printf("enter the bitpos \n");
-    scanf("%d",&bitpos);
-
-    num = (num|(1<<bitpos));
+    num = (num|(1<<pos));
 
     return num;
 }
 
 void print_bits(int num)
 {
+    int count = 0;
     for(int i = 31;i>=0;i--)
+    {
+        count++;
         printf("%d ",num>>i&1);
-
-    printf("\n"); //for next line 
+        if(count == 4)
+        {
+            printf(" "),
+            count = 0;
+        }
+    }
 }
 
 int main()
 {
     printf("Let's Start \n");
-    int num = 10,mod;
-    mod = set_bit(num);
+    int num = 10,pos=2;
+    num = set_bit(num,pos);
 
-    print_bits(mod);
+    print_bits(num);
 
     return 0;
 }
